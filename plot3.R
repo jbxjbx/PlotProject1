203351ddacb72995c1c388dd7665166fb52ad895
Boxiang Jia <jbxjbxjbxjbxjbx@hotmail.com>
2016-07-03 14:14:30 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,26 +1,26 @@
-setwd("./Data_Science/Course_04/Week01/EConsumption")
-path<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(path,destfile = "EConsumption.zip" )
-
-filePath<-"household_power_consumption.txt"
-ECData1<-read.table(filePath,header=T,na.strings =NA,sep=";" )
-ECData1$Date<-as.Date(ECData1$Date,format="%d/%m/%Y")
-head(ECData1)
-ECData<-subset(ECData1,Date=="2007/02/01"| Date=="2007/02/02")
-library(lubridate)
-ECData$DateTime<- ymd_hms(paste(ECData$Date,ECData$Time))
-
-
-ECData$Sub_metering_1<-as.numeric(ECData$Sub_metering_1)
-ECData$Sub_metering_2<-as.numeric(ECData$Sub_metering_2)
-ECData$Sub_metering_3<-as.numeric(ECData$Sub_metering_3)
-
-par(mfrow=c(1,1))
-
-plot(ECData$DateTime,ECData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-lines(ECData$DateTime,ECData$Sub_metering_2, type="l",col="red")
-lines(ECData$DateTime,ECData$Sub_metering_3, type="l",col="blue")
-legend("topright",lty=c(1,1,1),lwd=c(2,2,2),col=c("black","red","blue"),
-       legend =c("Sub metering 1","Sub metering 2","Sub metering 3") )
-dev.copy(png, file="plot3.png", width=480, height=480)
-dev.off()
+plot3 <- function(){
+        # load library and dataset
+        library(dataplot)
+        data <- read.table("household_power_consumption.txt",header = TRUE, sep = ";",na.strings = "?")
+        
+        # clean the data set
+        
+        cleandata <- subset(cleandata,Date == "1/2/2007"| Date == "2/2/2007" )
+        cleandata$Date <- as.Date(cleandata$Date, format = "%d/%m/%Y")
+        cleandata$Time <- paste(cleanata$Date,cleandata$Time,sep = " ")
+        cleandata$Time <- ymd_hms(cleandata$Time)
+        
+        # set the plotting device to png file
+        png(filename = "./plot3.png",width = 480, height = 480)
+        
+        # plot graph
+        with(cleandata,plot(Sub_metering_1 ~ Time,type = "l",xlab= " ",
+             ylab = "Energy sub metering" ))
+        lines(x=cleandata$Time,y=cleandata$Sub_metering_2,col= "red")
+        lines(x=cleandata$Time,y=cleandata$Sub_metering_3,col= "blue")
+        legend("topright",lty = 1,lwd= 2,col=c("black","red","blue"),
+               legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+        
+        # close png device
+        dev.off()
+}
